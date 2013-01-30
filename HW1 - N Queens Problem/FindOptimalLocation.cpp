ce8a7a18e6667cd146dbb415560ac72b6308decf
Francesca Nannizzi <fnannizzi@gmail.com>
2013-01-30 02:46:07 -0800

    Starting to build a program to find optimal location for 1st queen

---
+++ HW1 - N Queens Problem/FindOptimalLocation.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){ //Attempt to optimize or find pattern in the placement of the first queen
+
+	int n; 
+
+	cout << "Enter a number between 0 and 14: " << endl;
+	cin >> n;
+	cout << "You have entered: " << n << endl;
+
+	int** board = new int*[n];
+	for(int i = 0; i < n; i++){
+		board[i] = new int[n];
+	}
+
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			board[i][j] = 0;
+		}
+	}
+
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			cout << board[i][j] << "   ";
+		}
+		cout << endl;
+	}	
+
+
+	/*Cases
+		- 0 no queens
+		- 1 1 queen
+		- 2 no queens
+		- 3 no queens
+		- 4 no queens
+		- 5 no queens
+		- 6 ??
+
+
+
+	return 0;
+}
