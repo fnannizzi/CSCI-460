324bf41a29c9f18798905e2b62a8d40a9b9bc4a1
Francesca Nannizzi <fnannizzi@gmail.com>
2013-02-15 00:30:26 -0800

    Debugging further

--- HW1 - N Queens Problem/Main.cpp
+++ HW1 - N Queens Problem/Main.cpp
@@ -4,13 +4,20 @@
 // - - - - - - - - - - - - - - - - - - - - - - -	| 
 
 #include <iostream>
+#include <fstream>
 #include <iomanip>
 #include <vector>
 
 using namespace std;
 
+// Define position struct
+struct coordinate {
+	int x, y;
+}; 
+
 // Increment y or x position marker
-void updatePosition(int &y, int &x, int N){
+bool updatePosition(int &y, int &x, int N, int** b, vector<coordinate> &tP){
+	bool check = true;
 	if(y < (N-1)){
 		if(x < (N-1)){
 			x++;
@@ -25,17 +32,29 @@ void updatePosition(int &y, int &x, int N){
 			x++;
 		}
 	}
+	
+	if(b[y][x] != 99){
+		check = false;
+	}
+	
+	for(int i = 0; i < tP.size(); i++){
+		if((tP[i].x == x) && (tP[i].y == y)){
+			check = false;
+			break;
+		}
+	}
+	return check;
 }
 
 // Mark the column and row spaces as unavailable
 void columnsAndRows(int y, int x, int **b, int nQ, int N){ 
 	for(int i = 0; i < N; i++){
 		if(b[y][i] == 99){
-			cout << "Removing rows " << i << endl;
+			//cout << "Removing rows " << i << endl;
 			b[y][i] = nQ;
 		}
 		if(b[i][x] == 99){
-			cout << "Removing columns " << i << endl;
+			//cout << "Removing columns " << i << endl;
 			b[i][x] = nQ;
 		}
 	}
@@ -45,7 +64,7 @@ void columnsAndRows(int y, int x, int **b, int nQ, int N){
 void checkDiagonalMoves(int y, int x, int **b, int nQ, int N, int yChange, int xChange){ 
 	while((y >= 0) && (x >= 0) && (y < N) && (x < N)){
 		if(b[y][x] == 99){
-			cout << "Removing diagonals " << x << y << endl;
+			//cout << "Removing diagonals " << x << y << endl;
 			b[y][x] = nQ;
 		}
 		y += yChange;
@@ -83,7 +102,7 @@ void checkKnightMoves(int y, int x, int **b, int nQ, int N){
 	if((y >= 0) && (y < N)){
 		if((x >= 0) && (x < N)){
 			if(b[y][x] == 99){
-				cout << "Removing knights " << x << y << endl;
+				//cout << "Removing knights " << x << y << endl;
 				b[y][x] = nQ;	
 			}
 		}
@@ -127,6 +146,23 @@ void knightMoves(int y, int x, int **b, int nQ, int N){
 	checkKnightMoves(yShift, xShift, b, nQ, N);
 }
 
+bool checkRemainingPositions(int **b, int N, vector<coordinate> tP){
+	bool positionAvailable = false;
+	for(int i = 0; i < N; i++){
+		for(int j = 0; j < N; j++){
+			if(b[i][j] == 99){ // position empty
+				for(int k = 0; k < tP.size(); k++){
+					if((i == tP[k].y) && (j == tP[k].x)){}
+					else {
+						return true;
+					}
+				}
+			}
+		}
+	}
+	return positionAvailable;
+}
+
 // Print the board (for testing)
 void printArray(int **b, int N){
 	for(int i = 0; i < N; i++){
@@ -140,24 +176,39 @@ void printArray(int **b, int N){
 }
 
 // Print the solution array (for testing)
-void printSolution(int *qP, int N){
-	for(int i = 0; i < N; i++){
-		cout << "(" << qP[(i*2) + 1] << "," << qP[i*2] << "), ";
+void printSolution(vector<coordinate> qP){
+	for(int i = 0; i < qP.size(); i++){
+		cout << "(" << qP[i].x << "," << qP[i].y << "), ";
 	}
 	cout << endl;
 }
 
 // Print multiple solutions (for testing)
-void printAllSolutions(vector<int*> &s, int N){
-	int* sArr;  
+void printAllSolutions(vector<vector<coordinate> > &s){ 
 	for(int i = 0; i < s.size(); i++){
-		sArr = s[i];
 		cout << "Solution " << (i + 1) << ":" << endl;
-		for(int j = 0; j < N; j++){
-			cout << "(" << sArr[j*2] << "," << sArr[(j*2) + 1] << "), ";
+		for(int j = 0; j < s[i].size(); j++){
+			cout << "(" << s[i][j].x << "," << s[i][j].y << "), ";
 		}
 		cout << endl;
 	}	
+}
+
+// Write the output to a file
+void writeSolutionsToFile(string f, vector<vector<coordinate> > &s){
+	ofstream file;
+	file.open(f.c_str());
+	for(int i = 0; i < s.size(); i++){
+		file << "Solution " << (i + 1) << ":\n";
+		for(int j = 0; j < s[i].size(); j++){
+			file << "(" << s[i][j].x << "," << s[i][j].y << ")";
+			if(j != (s[i].size() - 1)){
+				file << ", ";
+			}
+		}
+		file << "\n";
+	}
+	file.close();
 }
 
 // Main 
@@ -166,11 +217,10 @@ int main(int argc, char *argv[]){
 	// Variable Declarations
 	int n, numSolutions = 0, numQueens = 0;
 	int yPos = 0, xPos = -1;
-	int yStart = 0, xStart = 0;
-	bool solutionsRemaining = true, initialTrace = true, beginBacktrace = false;
-	bool solutionsFound = false;
+	bool solutionsRemaining = true, positionFound = false;
 	int numSolutionsRecorded = 0;
 	int count = 0;
+	string filename = "";
 
 	// Handling command line arguments
 	if(argc == 0){ // if user forgets to specify n, ask them
@@ -181,6 +231,8 @@ int main(int argc, char *argv[]){
 	else { // otherwise, process the arguments
 		n = atoi(argv[1]);
 		cout << n << endl;
+		
+		filename = argv[2];
 	}
 
 	if((n >= 0) && (n < 15)){ // check to see if n is valid
@@ -197,82 +249,78 @@ int main(int argc, char *argv[]){
 				board[i][j] = 99;
 			}
 		}
+		
+		coordinate blank;
+		blank.x = -1;
+		blank.y = -1;
 
-		// Initialize 2D solution array
-		int* queenPositions = new int[n*2];
-		for(int i = 0; i < (n*2); i++){
-			queenPositions[i] = -1;
-		}
+		// Initialize current positions vector
+		vector<coordinate> queenPositions (n, blank);
 
 		// Initialize solutions vector
-		vector<int*> solutions;
+		vector<vector<coordinate> > solutions;
+		
+		// Initialize tried positions vector
+		vector<coordinate> triedPositions;
 		
 		// Solution algorithm
-		while(solutionsRemaining){
-			while(!solutionsFound){
-				initialTrace = true;
-				while(initialTrace){
-					while(!positionFound){
-						updatePosition(yPos, xPos, n);
-						if(board[yPos][xPos] == 99){
-							board[yPos][xPos] = (40 + numQueens);
-							queenPositions[numQueens*2] = yPos;
-							queenPositions[(numQueens*2) + 1] = xPos;	
-							printSolution(queenPositions, n);	
+		while(solutionsRemaining){ // runs until every possibility has been searched
+			positionFound = updatePosition(yPos, xPos, n, board, triedPositions);
+			cout << "positionFound " << positionFound << endl;
+			if(positionFound){ // position available
+				board[yPos][xPos] = (40 + numQueens);
+				queenPositions[numQueens].y = yPos;
+				queenPositions[numQueens].x = xPos;	
+				// Make spaces taken by horizontal or vertical moves unavailable
+				columnsAndRows(yPos, xPos, board, numQueens, n); 
+			
+				// Make spaces taken by diagonal moves unavailable
+				diagonalMoves(yPos, xPos, board, numQueens, n);
+
+				// Make spaces taken by knight moves unavailable			
+				knightMoves(yPos, xPos, board, numQueens, n);
+				
+				numQueens++;	
+				cout << "numQueens " << numQueens << endl;
+			}
+			else {
+				cout << "Position not found" << endl;
+				if((yPos == (n-1)) && (xPos == (n-1))){	// have checked last location	
+					if(numQueens == n){ // if there are n queens on the board
+						cout << "SOLUTION FOUND!" << endl;
+						solutions.push_back(queenPositions);
+						numSolutionsRecorded++;
+						printSolution(queenPositions);							
+					}
+					else { // not a solution, time to backtrack
+						positionFound = checkRemainingPositions(board, n, triedPositions);	
+						if(!positionFound){ // no untried positions
+							triedPositions.clear();
 						}
-						else {
-							if((yPos == (n-1)) && (xPos == (n-1))){		
-								if(numQueens == n){
-									solutions.push_back(queenPositions);
-									printAllSolutions(solutions, n);
-									solutionFound = true;							
-									break;
-								}
-								else {
-									cout << "beginning backtrace" << endl;
-									beginBacktrace = true;
-									updatePosition(yPos, xPos, n);
-									for(int i = 0; i < n; i++){
-										for(int j = 0; j < n; j++){
-											if(board[i][j] == numQueens){
-												board[i][j] = 99;
-											}
-										}
-									}
-									numQueens--;	
-									printSolution(queenPositions, n);
-									board[queenPositions[numQueens*2]][queenPositions[(numQueens*2) + 1]] = 99;
-									queenPositions[numQueens*2] = -1;
-									queenPositions[(numQueens*2) + 1] = -1;
-									posFound = true;
-								}
+					}
+					coordinate c;
+					c.x = queenPositions[numQueens].x;
+					c.y = queenPositions[numQueens].y;
+					cout << "Failed: " << c.x << " " << c.y << endl;					
+					triedPositions.push_back(c);
+					board[c.y][c.x] = 99;
+					for(int i = 0; i < n; i++){
+						for(int j = 0; j < n; j++){
+							if(board[i][j] == numQueens){
+								board[i][j] = 99;
 							}
 						}
 					}
-					// Make spaces taken by horizontal or vertical moves unavailable
-					columnsAndRows(yPos, xPos, board, numQueens, n); 
-			
-					// Make spaces taken by diagonal moves unavailable
-					diagonalMoves(yPos, xPos, board, numQueens, n);
-
-					// Make spaces taken by knight moves unavailable			
-					knightMoves(yPos, xPos, board, numQueens, n);
-
-					numQueens++;
-					count++;
-					cout << count << endl;
-					printArray(board, n);
-					if(count == 11){
-						break;
-					}	
+					numQueens--;
+					if(numQueens == 0){
+						if((c.x == (n-1)) && (c.y == (n-1))){
+							solutionsRemaining = false;
+						}
+					}
 				}
 			}
-			if((yStart == (n-1)) && (xStart == (n-1))){
-
-			}
-			updatePosition(yStart, xStart, n);
-			solutionsFound = false;
 		}
-	}
+		writeSolutionsToFile(filename, solutions);
+	}	
 	return 0;
 }
