a602b461038fd70d2e003521b62dfa5470762045
Francesca Nannizzi <fnannizzi@gmail.com>
2013-02-27 05:02:28 -0800

    Added Node class

--- HW2 - Nine Tile Puzzle/PuzzleSolver.cpp
+++ HW2 - Nine Tile Puzzle/PuzzleSolver.cpp
@@ -7,23 +7,9 @@
 #include <fstream>
 #include <iomanip>
 #include <vector>
+#include "Node.h"
 
 using namespace std;
-
-struct tile {
-	int value;
-	int position;
-	int numLinks;
-	int* links;
-	
-	// Constructor
-	tile(int v, int p, int nL){
-		value = v;
-		position = p;
-		numLinks = nL;
-		links = new int[numLinks];
-	}
-};
 
 // Read the input from a file
 void readFromInputFile(string f, vector<int> &iV){
@@ -38,123 +24,8 @@ void readFromInputFile(string f, vector<int> &iV){
 	}
 }
 
-// Load the input into tile vector
-void loadInputIntoTiles(vector<int> iV, vector<tile> &t){
-	// Tile 1
-	tile a(iV[0], 0, 3);
-	t.push_back(a);
-	
-	//Tile 2
-	tile b(iV[1], 1, 4);
-	t.push_back(b);	
-	
-	//Tile 3
-	tile c(iV[2], 2, 3);
-	t.push_back(c);	
-	
-	//Tile 4
-	tile d(iV[3], 3, 3);
-	t.push_back(d);	
-
-	//Tile 5
-	tile e(iV[4], 4, 6);
-	t.push_back(e);
-	
-	//Tile 6
-	tile f(iV[5], 5, 6);
-	t.push_back(f);	
-			
-	//Tile 7
-	tile g(iV[6], 6, 3);
-	t.push_back(g);	
-			
-	//Tile 8
-	tile h(iV[7], 7, 3);
-	t.push_back(h);	
-		
-	//Tile 9
-	tile i(iV[8], 8, 4);
-	t.push_back(i);
-	
-	//Tile 10
-	tile j(iV[9], 9, 3);
-	t.push_back(j);	
-}
-
-// Add edges/links between tiles
-void addEdgesToTiles(vector<tile> &t){
-	t[0].links[0] = 3;
-	t[0].links[1] = 4;
-	t[0].links[2] = 1;
-	
-	t[1].links[0] = 4;
-	t[1].links[1] = 5;
-	t[1].links[2] = 0;
-	t[1].links[3] = 2;
-	
-	t[2].links[0] = 5;
-	t[2].links[1] = 6;
-	t[2].links[2] = 1;
-	
-	t[3].links[0] = 0;
-	t[3].links[1] = 4;
-	t[3].links[2] = 7;
-	
-	t[4].links[0] = 0;
-	t[4].links[1] = 1;
-	t[4].links[2] = 5;
-	t[4].links[3] = 8;
-	t[4].links[4] = 7;
-	t[4].links[5] = 3;
-	
-	t[5].links[0] = 1;
-	t[5].links[1] = 2;
-	t[5].links[2] = 6;
-	t[5].links[3] = 9;
-	t[5].links[4] = 8;
-	t[5].links[5] = 4;
-	
-	t[6].links[0] = 2;
-	t[6].links[1] = 5;
-	t[6].links[2] = 9;
-	
-	t[7].links[0] = 3;
-	t[7].links[1] = 4;
-	t[7].links[2] = 8;
-	
-	t[8].links[0] = 4;
-	t[8].links[1] = 5;
-	t[8].links[2] = 7;
-	t[8].links[3] = 9;
-	
-	t[9].links[0] = 5;
-	t[9].links[1] = 6;
-	t[9].links[2] = 8;
-}
-
-// Print the board (for testing)
-void printBoard(vector<tile> t){
-	cout << "  ";
-	for(int i = 0; i < 3; i++){ // Print row 1
-		cout << setw(3) << t[i].value;
-	}
-	cout << endl;
-
-	for(int i = 3; i < 7; i++){ // Print row 2
-		cout << setw(3) << t[i].value;
-	}
-	cout << endl;
-	
-	cout << "  ";
-	for(int i = 7; i < 10; i++){ // Print row 3
-		cout << setw(3) << t[i].value;
-	}
-	cout << endl;	
-	
-}
-
 // Print the solution and number of moves
-void printSolution(vector<int> moves, int numMoves){
+void printSolutionOutOfPlace(vector<int> moves, int numMoves){
 	cout << "Number of Misplaced Tiles: " << endl;
 	cout << "Sequence of tiles to be moved: ";
 	for(int i = 0; i < moves.size(); i++){
@@ -164,63 +35,14 @@ void printSolution(vector<int> moves, int numMoves){
 	cout << "Number of moves: " << numMoves << endl;
 }
 
-// Calculate number of tiles out of place relative to goal state
-int tilesOutOfPlaceRelativeToGoal(vector<tile> t){
-	int numTiles = 0;
-	for(int i = 0; i < 10; i++){
-		if(t[i].value != (i + 1)){
-			if(i != 9){
-				numTiles++;
-			}
-			else if(t[i].value != 0){
-				numTiles++;
-			}
-		}
-	}
-	return numTiles;
-}
-
-/*// Calculate Manhattan distance to goal from particular tile
-int manhattanDistance(vector<tile> t, int tileIndex){
-	int distance = 0;
-	if(t[tileIndex].value == 
-}*/
-
-// Move a tile by one position
-void moveTilePosition(vector<tile> &t, int startPosition, int endPosition){
-	if(t[endPosition].value == 0){
-		for(int i = 0; i < t[startPosition].numLinks; i++){
-			if(t[startPosition].links[i] == endPosition){
-				int temp = t[startPosition].value;
-				t[startPosition].value = 0;
-				t[endPosition].value = temp;
-				break;
-			}
-		}
-	}
-	else {
-		cout << "ERROR: Cannot move tile." << endl;
-	}
-}
-
-// Determines the index of the empty space
-int locateEmptySpace(vector<tile> &t){
-	for(int i = 0; i < 10; i++){
-		if(t[i].value == 0){
-			return i;
-		}
-	}
-	return -1;
-}
-
 // Make next move based on number of tiles out of place relative to goal
-void nextMove(vector<int> &moves, int &numMoves, vector<tile> &t){
-	int position = locateEmptySpace(t);
+void nextMove(vector<int> &moves, int &numMoves, Node &p){
+	int position = p.locateEmptySpace();
 	if(position < 9){
-		vector<vector<tile> > nodes;
-		for(int i = 0; i < t[position].numLinks; i++){
-			vector<tile> node = t; 
-			moveTilePosition(node, t[position].links[i], position);
+		vector<Node> nodes;
+		for(int i = 0; i < p.board[position].numLinks; i++){
+			Node node = p; 
+			node.moveTilePosition(p.board[position].links[i], position);
 			nodes.push_back(node);
 			//cout << "Node " << i << endl;
 			//printBoard(node);
@@ -228,16 +50,16 @@ void nextMove(vector<int> &moves, int &numMoves, vector<tile> &t){
 		}
 		int indexBestChoice = 0;
 		for(int i = 0; i < nodes.size(); i++){
-			if(tilesOutOfPlaceRelativeToGoal(nodes[i]) < 
-			   tilesOutOfPlaceRelativeToGoal(nodes[indexBestChoice])){
+			if(nodes[i].tilesOutOfPlaceRelativeToGoal() < 
+			   nodes[indexBestChoice].tilesOutOfPlaceRelativeToGoal()){
 				indexBestChoice = i;
+				cout << "indexBestChoice: " << i << endl;
 			}
 		}
-		moveTilePosition(t, t[position].links[indexBestChoice], position);
-		moves.push_back(t[position].value);
+		p.moveTilePosition(p.board[position].links[indexBestChoice], position);
+		moves.push_back(p.board[position].value);
 		numMoves++;
-	}
-	
+	}	
 }
 
 // Main 
@@ -246,7 +68,6 @@ int main(int argc, char *argv[]){
 	// Variable Declarations
 	string filename = "";
 	vector<int> inputValues;
-	vector<tile> tiles;
 	vector<int> moves;
 	int numMoves = 0;
 
@@ -261,19 +82,24 @@ int main(int argc, char *argv[]){
 
 	// Initialize board
 	readFromInputFile(filename, inputValues);
-	loadInputIntoTiles(inputValues, tiles);
-	addEdgesToTiles(tiles);
+	Node puzzle(inputValues);
+	
+	
+	
 	
 	// Find a solution with heuristic 2: 
 	// number of tiles out of place relative to goal
-	while(tilesOutOfPlaceRelativeToGoal(tiles) > 0){
-		//cout << "num tiles out of place" << tilesOutOfPlaceRelativeToGoal(tiles) << endl;
-		nextMove(moves, numMoves, tiles);
-		//cout << "Decision: " << endl;
-		//printBoard(tiles);
-		//cout << endl;
+	while(puzzle.tilesOutOfPlaceRelativeToGoal() > 0){
+		nextMove(moves, numMoves, puzzle);
+		cout << "Decision: " << endl;
+		puzzle.printBoard();
+		cout << endl;
+		cout << "Number of moves: " << numMoves << endl;
+		if(numMoves > 12){
+			break;
+		}
 	}
-	printSolution(moves, numMoves);
+	printSolutionOutOfPlace(moves, numMoves);
 		
 	return 0;
 } // end main
